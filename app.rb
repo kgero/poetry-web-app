b422e166130eb7e68b66075a3ed574d96bcef219
Katy Gero <katystreet@gmail.com>
2016-09-22 16:30:01 -0400

    read from relational db, print close poems, footer

--- app.rb
+++ app.rb
@@ -2,17 +2,25 @@ require 'sinatra'
 require 'sequel'
 require 'erb'
 
-DB = Sequel.connect('sqlite://testpoems.db')
+DB = Sequel.connect('sqlite://poem_info.db')
 poems = DB[:poems]
+poem_distances = DB[:poem_distances]
 
 get '/' do
-	@foo = "Hello, World!"
-	@poems = poems
+	rand_nums = 10.times.map{ Random.rand(poems.count) } 
+	@foo = "Or select a poem from random. Refresh the page to get another random set of poems."
+	@poems = poems.where(:poem_num => rand_nums)
 	erb :index
 end
 
+get '/howitworks' do
+	erb :howitworks
+end
+
 get '/poem/:id' do
-	@poem = poems.where(:id => params['id']).first
+	@poem = poems.where(:poem_num => params['id']).first
+	@poems = poems
+	@relations = poem_distances.where(:poem1=>params['id']).or(:poem2=>params['id']).order(:distance).limit(10)
 	erb :poem
 end
 
@@ -20,9 +28,8 @@ post '/submit' do
 	query = params[:query]
 	@poems = poems.where(:poet => query)
 	@foo = "results..."
-	@helper = ""
 	if @poems.count == 0
-		@helper = "None found. Try Robert Frost."
+		@foo = "None found. Try Robert Frost."
 	end
 	erb :index
 end

0956691da860b04488703bd77c8c050114ce9566
Katy Gero <katystreet@gmail.com>
2016-10-01 10:16:17 -0400

    use id as primary key

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require 'sinatra'
 require 'sequel'
 require 'erb'
 
-DB = Sequel.connect('sqlite://poem_info.db')
-poems = DB[:poems]
+DB = Sequel.connect('sqlite://poemdb2.db')
+poems = DB[:poetry]
 poem_distances = DB[:poem_distances]
 
 get '/' do
 	rand_nums = 10.times.map{ Random.rand(poems.count) } 
 	@foo = "Or select a poem from random. Refresh the page to get another random set of poems."
-	@poems = poems.where(:poem_num => rand_nums)
+	@poems = poems.where(:id => rand_nums)
 	erb :index
 end
 
@@ -18,7 +18,7 @@ get '/howitworks' do
 end
 
 get '/poem/:id' do
-	@poem = poems.where(:poem_num => params['id']).first
+	@poem = poems.where(:id => params['id']).first
 	@poems = poems
 	@relations = poem_distances.where(:poem1=>params['id']).or(:poem2=>params['id']).order(:distance).limit(10)
 	erb :poem

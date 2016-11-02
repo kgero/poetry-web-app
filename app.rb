4a3f92a9b664e5766edb5e3ddabcaa04690b6848
Katy Gero <katystreet@gmail.com>
2016-11-01 21:21:00 -0400

    new db structure

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'erb'
 DB = Sequel.connect(ENV['DATABASE_URL'] || "postgres://qvigtqbjcjujkq:2aTeOgnlW1NuhT3SGAEcPpNzh6@ec2-54-243-30-73.compute-1.amazonaws.com:5432/d14pjbfuk22qfv")
 # DB = Sequel.connect('sqlite://poemdb2.db')
 poems = DB[:poetry]
-poem_distances = DB[:poem_distances]
+# poem_distances = DB[:poem_distances]
 
 get '/' do
 	rand_nums = 10.times.map{ Random.rand(poems.count) } 
@@ -23,8 +23,8 @@ get '/poem/:id' do
 	@poems = poems
 	@poem = poems.where(:id => params['id']).first
 
-	entry = poem_distances.where(:poem1=>params['id']).or(:poem2=>params['id']).order(:distance).first
-	@rec_poem = poems.where(:id => entry[:poem1]).or(:id => entry[:poem2]).exclude(:id => @poem[:id]).first
+	# entry = poem_distances.where(:poem1=>params['id']).or(:poem2=>params['id']).order(:distance).first
+	@rec_poem = poems.where(:id => @poem[:close_poem]).first
 
 	@poem_content = @poem[:poem].gsub(/\n/, '<br>')
 	@rec_content = @rec_poem[:poem].gsub(/\n/, '<br>')

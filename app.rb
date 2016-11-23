838051511fe3990bcf45e00feeca653ca3d6d1a0
Katy Gero <katystreet@gmail.com>
2016-11-22 22:52:14 -0500

    trying to pass ruby var to javascript

--- app.rb
+++ app.rb
@@ -4,13 +4,11 @@ require 'pg'
 require 'erb'
 
 DB = Sequel.connect(ENV['DATABASE_URL'] || "postgres://qvigtqbjcjujkq:2aTeOgnlW1NuhT3SGAEcPpNzh6@ec2-54-243-30-73.compute-1.amazonaws.com:5432/d14pjbfuk22qfv")
-# DB = Sequel.connect('sqlite://poemdb2.db')
 poems = DB[:poetry]
-# poem_distances = DB[:poem_distances]
+topics = DB[:topics]
 
 # TODO:
 # get surprise poem to send you to a random poem page
-# be able to click on an author and get a list of all poems by that author
 # make the nav bar better :'( :'(
 # better separate selected and recommended poem?
 
@@ -30,7 +28,6 @@ get '/poem/:id' do
 	@poem = poems.where(:id => params['id']).first
 	
 	if @poem[:close_poem] != nil
-		# entry = poem_distances.where(:poem1=>params['id']).or(:poem2=>params['id']).order(:distance).first
 		@rec_poem = poems.where(:id => @poem[:close_poem]).first
 
 		@poem_content = @poem[:poem].gsub(/\n/, '<br>')
@@ -41,7 +38,8 @@ get '/poem/:id' do
 		@rec_content = "Sorry, we haven't run the algorithm on that poem yet!"
 	end
 
-	# @relations = poem_distances.where(:poem1=>params['id']).or(:poem2=>params['id']).order(:distance).limit(10)
+	@topic1 = ['the', 'i', 'poem']
+
 	erb :poem
 end
 
